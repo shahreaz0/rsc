5abf946e28dff4793fbe9008c1e033edc1179a3f
shahreaz0 <ashahreaz@gmail.com>
2025-04-20 23:05:36 +0600

    initial commit

---
+++ apis/users.ts
@@ -0,0 +1,39 @@
+export type UserResponse = {
+  first: number;
+  prev: null;
+  next: number;
+  last: number;
+  pages: number;
+  items: number;
+  data: User[];
+};
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  createdAt: Date;
+};
+
+export async function getUserList({ _page = 1, _per_page = 10 } = {}) {
+  const params = new URLSearchParams({
+    _page: _page.toString(),
+    _per_page: _per_page.toString(),
+  }).toString();
+
+  //
+
+  const res = await fetch(`http://localhost:8787/users?${params}`, {
+    // cache: "force-cache",
+    // next: {
+    //   revalidate: 5,
+    // },
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data: UserResponse = await res.json();
+  return data;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,62 @@
-import Image from "next/image";
+import { getUserList } from "@/apis/users";
+import Link from "next/link";
 
-export default function Home() {
+type ServerProps = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default async function Home({ searchParams }: ServerProps) {
+  const sp = await searchParams;
+
+  const page = typeof sp.page === "string" ? Math.max(+sp.page, 1) : 1;
+
+  const users = await getUserList({ _page: page });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="m-5 p-5">
+      <h1>Users</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className="overflow-y-auto h-[calc(100vh-40px)]">
+        <table className="table-auto w-full">
+          <thead>
+            <tr>
+              <th className="border">ID</th>
+              <th className="border">Name</th>
+              <th className="border">Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.data.map((user) => (
+              <tr key={user.id}>
+                <td className="border">{user.id}</td>
+                <td className="border">{user.name}</td>
+                <td className="border">{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="space-x-2">
+          <Link
+            href={`/?page=${users.pages < page ? users.pages - 1 : page - 1}`}
+            style={{
+              pointerEvents: !users.prev ? "none" : "auto",
+              opacity: !users.prev ? 0.5 : 1,
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Prev
+          </Link>
+          <Link
+            href={`/?page=${page + 1}`}
+            style={{
+              pointerEvents: !users.next ? "none" : "auto",
+              opacity: !users.next ? 0.5 : 1,
+            }}
           >
-            Read our docs
-          </a>
+            Next
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
